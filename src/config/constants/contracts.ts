4b7492a14024730fa2e26b8d438098bc1196f7da
snappycreeper <anhochanadel@gmail.com>
2021-08-28 18:20:37 -0500

    Initial commit

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,42 @@
+export default {
+  cake: {
+    1666600000: '0x28Cb748381F41b3DE3A5Dc70323F78BFC6fDadef',
+    97: '',
+  },
+  masterChef: {
+    1666600000: '0xe06d418DA58a769812e79fe8dF20AC1340B5Cc42',
+    97: '',
+  },
+  wbnb: {
+    // this is WONE "0xcf664087a5bb0237a0bad6742852ec6c8d69a27a"
+    1666600000: '0xcf664087a5bb0237a0bad6742852ec6c8d69a27a',
+    97: '',
+  },
+  lottery: {
+    1666600000: '',
+    97: '',
+  },
+  lotteryNFT: {
+    1666600000: '',
+    97: '',
+  },
+  mulltiCall: {
+    1666600000: '0xFE4980f62D708c2A84D3929859Ea226340759320',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    // this is BUSD "0xe176ebe47d621b984a73036b9da5d834411ef734"
+    1666600000: '0xe176ebe47d621b984a73036b9da5d834411ef734',
+    97: '',
+  },
+  viper: {
+    // this is VIPER "0xea589e93ff18b1a1f1e9bac7ef3e86ab62addc79"
+    1666600000: '0xea589e93ff18b1a1f1e9bac7ef3e86ab62addc79',
+    97: '',
+  },
+  xya: {
+    // this is XYA "0x9b68BF4bF89c115c721105eaf6BD5164aFcc51E4"
+    1666600000: '0x9b68BF4bF89c115c721105eaf6BD5164aFcc51E4',
+    97: '',
+  },
+}
